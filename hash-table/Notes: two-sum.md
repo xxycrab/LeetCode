5e4cdf1e1d24673c9778e95b942daa82581ab744
Xinyu Xie <xiexinyucrab@126.com>
2025-03-08 18:24:21 -0800

    :ballot_box_with_check: two-sum [Runtime: 4 ms; Memory: 44.7 MB]

--- hash-table/Notes: two-sum.md
+++ hash-table/Notes: two-sum.md
@@ -1,5 +1,5 @@
 ## [undefined. Two Sum](LeetCode link: https://leetcode.com/problems/undefined)
-$\color{red}Easy$ **[object Object]** [object Object]**
+$\color{red}Easy$ **[object Object]** **[object Object]
 <p>Given an array of integers <code>nums</code>&nbsp;and an integer <code>target</code>, return <em>indices of the two numbers such that they add up to <code>target</code></em>.</p>
 
 <p>You may assume that each input would have <strong><em>exactly</em> one solution</strong>, and you may not use the <em>same</em> element twice.</p>
--- hash-table/two-sum.java
+++ hash-table/two-sum.java
@@ -1,5 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
+        Map<Integer, Integer> hashmap = new HashMap<>();
+        for(int i = 0; i<nums.length; i++){
+            hashmap.put(nums[i], i);
+        }
+        int[] res = new int[]{};
+        for(int i = 0; i<nums.length; i++){
+            int complement = target - nums[i];
+            if (hashmap.containsKey(complement) && hashmap.get(complement) != i) {
+               return new int[] { i, hashmap.get(complement) };
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
