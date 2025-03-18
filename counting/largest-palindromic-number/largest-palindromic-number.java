339d350cd84a93f72efa3a27c7be7d4cdb52fbc5
Xinyu Xie <xiexinyucrab@126.com>
2025-03-18 00:30:47 -0700

    :ballot_box_with_check: largest-palindromic-number [Runtime: N/A; Memory: N/A]

--- counting/largest-palindromic-number/largest-palindromic-number.java
+++ counting/largest-palindromic-number/largest-palindromic-number.java
@@ -2,16 +2,16 @@ class Solution {
     public String largestPalindromic(String num) {
 
         int[] occurrence = new int[10];
-        for(char d : num.toCharArray()) {
-            occurrence[d-'0']++;
+        for (char d : num.toCharArray()) {
+            occurrence[d - '0']++;
         }
         int[] single = new int[10];
-        int maxSingle = 0;
+        int maxSingle = -1;
 
         StringBuilder sb = new StringBuilder();
-        for(int i = 9; i >=0; i--) {
+        for (int i = 9; i >= 0; i--) {
             int count = occurrence[i];
-            while(occurrence[i] > (count+1) / 2) {
+            while (occurrence[i] > (count + 1) / 2) {
                 occurrence[i]--;
                 sb.append(String.valueOf(i));
             }
@@ -19,20 +19,22 @@ class Solution {
                 occurrence[i]--;
                 maxSingle = Math.max(i, maxSingle);
                 single[i] = 1;
-            } 
+            }
         }
         if (sb.length() == 0 || sb.charAt(0) == '0') {
             return String.valueOf(maxSingle);
         } else {
-            sb.append(String.valueOf(maxSingle));
-            for(int i = 0; i<=9; i++) {
-                while(occurrence[i] > 0) {
+            if (maxSingle >= 0) {
+                sb.append(String.valueOf(maxSingle));
+            }
+            for (int i = 0; i <= 9; i++) {
+                while (occurrence[i] > 0) {
                     occurrence[i]--;
                     sb.append(String.valueOf(i));
                 }
             }
         }
         return sb.toString();
-        
+
     }
 }
