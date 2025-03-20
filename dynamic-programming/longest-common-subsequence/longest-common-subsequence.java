c7a7529986448690f22dcfe46834377029492451
Xinyu Xie <xiexinyucrab@126.com>
2025-03-19 23:33:14 -0700

    :ballot_box_with_check: longest-common-subsequence [Runtime: N/A; Memory: N/A]

--- dynamic-programming/longest-common-subsequence/longest-common-subsequence.java
+++ dynamic-programming/longest-common-subsequence/longest-common-subsequence.java
@@ -28,19 +28,22 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int m = text1.length(), n = text2.length();
         int[] lcs = new int[n];
+        int[] last = lcs;
         lcs[0] = text1.charAt(0) == text2.charAt(0) ? 1 : 0;
         for (int i = 1; i < n; i++) {
-            lcs[i] = text1.charAt(0) == text2.charAt(i) ? 1 : lcs[i-1];
+            last[i] = text1.charAt(0) == text2.charAt(i) ? 1 : lcs[i-1];
         }
 
         for (int i = 1; i < m; i++) {
-            int[] last = lcs;
             for (int j = 1; j < n; j++) {
                 if (text1.charAt(i) == text2.charAt(j)) {
                     lcs[j] = last[j - 1] + 1;
                 } else {
                     lcs[j] = Math.max(last[j], lcs[j - 1]);
                 }
+            }
+            for (int j = 0; j < n; j++) {
+                last[j] = lcs[j];
             }
         }
 
