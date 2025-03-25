644f764eeded764f08dbb30d8c3fccbbd95aa889
Xinyu Xie <xiexinyucrab@126.com>
2025-03-24 22:58:49 -0700

    :ballot_box_with_check: minimum-genetic-mutation [Runtime: N/A; Memory: N/A]

---
+++ hash-table/minimum-genetic-mutation/minimum-genetic-mutation.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int minMutation(String startGene, String endGene, String[] bank) {
+        Map<String, Queue<String>> validMutations = new HashMap<>();
+        validMutations.put(startGene, new LinkedList<String>());
+        for (String g : bank) {
+            if (validMutations.get(g) != null) {
+                continue;
+            }
+            validMutations.put(g, new LinkedList<String>());
+            if (this.isValidMutation(startGene, g)) {
+                validMutations.get(g).add(startGene);
+                validMutations.get(startGene).add(g);
+            }
+        }
+        for (String g : validMutations.keySet()) {
+            for (String h : validMutations.keySet()) {
+                if (g.equals(h))
+                    continue;
+                if (this.isValidMutation(g, h)) {
+                    validMutations.get(g).add(h);
+                }
+            }
+        }
+
+        Queue<String> q = new LinkedList<>();
+        q.add(startGene);
+        int length = 0;
+        while (!q.isEmpty()) {
+            String gene = q.poll();
+            length++;
+            while (!validMutations.get(gene).isEmpty()) {
+                String g = validMutations.get(gene).poll();
+                if (g.equals(endGene)) {
+                    return length;
+                } else {
+                    q.add(g);
+                }
+            }
+        }
+
+        return length > 0 ? length : -1;
+
+    }
+
+    private boolean isValidMutation(String startGene, String endGene) {
+        if (startGene.equals(endGene)) {
+            return false;
+        }
+        int diff = 0;
+        for (int i = 0; i < startGene.length(); i++) {
+            if (startGene.charAt(i) != endGene.charAt(i)) {
+                diff++;
+            }
+            if (diff > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
