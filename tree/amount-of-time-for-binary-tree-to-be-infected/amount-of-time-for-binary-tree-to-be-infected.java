78c0c4520ea5c6efd2ce92d4757d878ca8ebad1f
Xinyu Xie <xiexinyucrab@126.com>
2025-04-09 23:34:04 -0700

    :ballot_box_with_check: amount-of-time-for-binary-tree-to-be-infected [Runtime: N/A; Memory: N/A]

--- tree/amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
+++ tree/amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
@@ -15,21 +15,18 @@
  */
 class Solution {
     public int amountOfTime(TreeNode root, int start) {
-        if (root.val == start) {
-            return 0;
-        }
         int[] leftTraverse = traverse(root.left, start);
         int[] rightTraverse = traverse(root.right, start);
 
-        int leftHeight = leftTraverse[0]+1;
-        int rightHeight = rightTraverse[0]+1;
+        int leftHeight = leftTraverse[0];
+        int rightHeight = rightTraverse[0];
         System.out.println(leftTraverse[0]);
         System.out.println(leftTraverse[1]);
         if (leftTraverse[1] > 0) {
-            return Math.max(leftHeight - leftTraverse[1] + rightHeight -1, leftTraverse[1]-1);
+            return Math.max(leftHeight - leftTraverse[1] + rightHeight + 1, leftTraverse[1] - 1);
         }
         if (rightTraverse[1] > 0) {
-            return Math.max(rightHeight - rightTraverse[1] + leftHeight - 1, rightTraverse[1]-1);
+            return Math.max(rightHeight - rightTraverse[1] + leftHeight + 1, rightTraverse[1] - 1);
         }
         return Math.max(leftHeight, rightHeight);
     }
@@ -42,7 +39,7 @@ class Solution {
         int[] rightTraverse = traverse(root.right, start);
         int startHeight = 0;
         if (root.val == start) {
-            startHeight = 1+Math.max(leftTraverse[0], rightTraverse[0]);
+            startHeight = 1 + Math.max(leftTraverse[0], rightTraverse[0]);
         } else {
             startHeight = Math.max(leftTraverse[1], rightTraverse[1]);
         }
