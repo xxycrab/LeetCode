2664f623d5563ffc257d71c1b93e4fe8ab3774a7
Xinyu Xie <xiexinyucrab@126.com>
2025-04-24 23:17:21 -0700

    :ballot_box_with_check: the-number-of-beautiful-subsets [Runtime: 6 ms; Memory: 44.7 MB]

--- backtracking/the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.java
+++ backtracking/the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.java
@@ -17,29 +17,28 @@ class Solution {
             for (Map.Entry<Integer, Integer> subsetEntry : entry.getValue().entrySet()) {
                 subsets.add(new Pair<>(subsetEntry.getKey(), subsetEntry.getValue()));
             }
-            totalCount *= countBeautifulSubsets(subsets, subsets.size(), k, 0);
+            totalCount *= countBeautifulSubsets(subsets, k, 0);
         }
 
         return totalCount - 1;
     }
 
-    private int countBeautifulSubsets(ArrayList<Pair<Integer, Integer>> subsets, int numSubsets, int difference,
-            int i) {
+    private int countBeautifulSubsets(ArrayList<Pair<Integer, Integer>> subsets, int difference, int i) {
         // Base case: Return 1 for a subset of size 1
         if (i == subsets.size()) {
             return 1;
         }
 
         // Calculate subsets where the current subset is not taken
-        int skip = countBeautifulSubsets(subsets, numSubsets, difference, i + 1);
+        int skip = countBeautifulSubsets(subsets, difference, i + 1);
         // Calculate subsets where the current subset is taken
         int take = (1 << subsets.get(i).getValue()) - 1;
 
         // If next number has a 'difference', calculate subsets; otherwise, move to next
-        if (i + 1 < numSubsets && subsets.get(i + 1).getKey() - subsets.get(i).getKey() == difference) {
-            take *= countBeautifulSubsets(subsets, numSubsets, difference, i + 2);
+        if (i + 1 < subsets.size() && subsets.get(i + 1).getKey() - subsets.get(i).getKey() == difference) {
+            take *= countBeautifulSubsets(subsets, difference, i + 2);
         } else {
-            take *= countBeautifulSubsets(subsets, numSubsets, difference, i + 1);
+            take *= countBeautifulSubsets(subsets, difference, i + 1);
         }
 
         return skip + take; // Return total count of subsets
