7b1ee36e9221ff1bc78b29fcfdbc0f787be7e555
Xinyu Xie <xiexinyucrab@126.com>
2025-06-11 23:17:14 -0700

    :ballot_box_with_check: longest-valid-parentheses [Runtime: 4 ms; Memory: 43.1 MB]

---
+++ dynamic-programming/longest-valid-parentheses/longest-valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int maxLen = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                // top of stack is either index of cloest ( or 
+                // index of the last ) which results in invalid brackets
+                // example: (()()))(), when processes the 4th ), it can on longer constructs valid brackets and pushed to stack
+                // which is used to identify the edge between valid brackets
+                stack.pop();
+                if(stack.empty()) {
+                    stack.push(i);
+                    continue;
+                }
+                maxLen = Math.max(maxLen, i-stack.peek());
+            }
+        }
+        return maxLen;
+    }
+}
