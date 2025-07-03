45891b1550a39a1a1e2411356308798e2c713fc7
Xinyu Xie <xiexinyucrab@126.com>
2025-07-02 22:43:43 -0700

    :ballot_box_with_check: permutations [Runtime: 1 ms; Memory: 44.8 MB]

---
+++ backtracking/permutations/permutations.java
@@ -0,0 +1,59 @@
+
+import java.util.Map.Entry;
+class Solution {
+    // public List<List<Integer>> permute(int[] nums) {
+    //     List<List<Integer>> res = new ArrayList<>();
+    //     Map<Integer, Boolean> visited = new HashMap<>();
+    //     for(int i: nums) {
+    //         visited.put(i, false);
+    //     }
+    //     dfs(res, new ArrayList<>(), visited);
+    //     return res;
+
+    // }
+
+    // private void dfs(List<List<Integer>> res, List<Integer> cur, Map<Integer, Boolean> visited) {
+    //     if (cur.size() == visited.keySet().size()) {
+    //         res.add(new ArrayList<>(cur));
+    //         return;
+    //     }
+    //     for (Entry<Integer, Boolean> entry : visited.entrySet()) {
+    //         if (!entry.getValue()) {
+    //             cur.add(entry.getKey());
+    //             visited.put(entry.getKey(), true);
+    //             dfs(res, cur, visited);
+    //             visited.put(entry.getKey(), false);
+    //             cur.removeLast();
+    //         }
+    //     }
+    // }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+
+        List<Integer> output = new ArrayList<Integer>();
+        for (int num : nums) {
+            output.add(num);
+        }
+
+        int n = nums.length;
+        backtrack(n, output, res, 0);
+        return res;
+    }
+
+    public void backtrack(int n, List<Integer> output, List<List<Integer>> res, int first) {
+        // 所有数都填完了
+        if (first == n) {
+            res.add(new ArrayList<Integer>(output));
+        }
+        for (int i = first; i < n; i++) {
+            // 动态维护数组
+            Collections.swap(output, first, i);
+            // 继续递归填下一个数
+            backtrack(n, output, res, first + 1);
+            // 撤销操作
+            Collections.swap(output, first, i);
+        }
+    }
+}
+
