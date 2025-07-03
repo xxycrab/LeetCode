0412ffa2ac5418c81cb41cf9f6a0b1ffb3011e5e
Xinyu Xie <xiexinyucrab@126.com>
2025-07-02 23:56:57 -0700

    :ballot_box_with_check: target-sum [Runtime: 10 ms; Memory: 44.9 MB]

--- backtracking/target-sum/target-sum.java
+++ backtracking/target-sum/target-sum.java
@@ -6,6 +6,9 @@ class Solution {
         }
         if (sum < target || -target > sum)
             return 0;
+        // dp[i][j] represents number of solutions that i elements can result in j. 0 <= j <= 2*sum, here dp[i][0] represents the case of elements resulting in -sum, etc.
+        // dp[i][j] = dp[i-1][j-nums[i]] + dp[i-1][j+nums[i]], as when considering nums[i] we can either add or substract
+        // We need to find dp[n][target+sum(nums)], i.e. the solutions that result in target+sum
         int[] dp = new int[2 * sum + 1];
         dp[nums[0] + sum] += 1;
         dp[sum - nums[0]] += 1;
